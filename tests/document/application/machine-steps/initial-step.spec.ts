f11b144a1b474b5a902062da114f9c10419092dd
ThaSMorato <tha.s.morato@gmail.com>
2024-11-24 22:59:58 -0300

    test: :white_check_mark: enhance InitialStep tests to validate document status transitions and error handling

--- tests/document/application/machine-steps/initial-step.spec.ts
+++ tests/document/application/machine-steps/initial-step.spec.ts
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker'
 
 import { InitialStep } from '@/document/application/machine-steps/initial-step'
+import { MachineStep } from '@/document/application/machine-steps/steps'
 import {
   DocumentStatus,
   DocumentType,
@@ -29,21 +30,43 @@ describe('InitialStep', () => {
     sut = new InitialStep()
   })
 
-  it('should return a right value if document is valid', async () => {
-    const result = await sut.handle(document)
+  describe('is right', () => {
+    it('should return a right value if document and phone is right', async () => {
+      const result = await sut.handle(document)
 
-    expect(result.isRight()).toBeTruthy()
+      expect(result.isRight()).toBeTruthy()
+    })
+
+    it('should point to DRAFT if status is DRAFT', async () => {
+      const result = await sut.handle({
+        ...document,
+        status: DocumentStatus.DRAFT,
+      })
+
+      expect(result.value.step).toBe(MachineStep.DRAFT)
+    })
+
+    it('should point to PENDING if status is not DRAFT', async () => {
+      const result = await sut.handle({
+        ...document,
+        status: DocumentStatus.APPROVED,
+      })
+
+      expect(result.value.step).toBe(MachineStep.PENDING)
+    })
   })
 
-  it('should return a left value if document is invalid', async () => {
-    const result = await sut.handle({ ...document, document: 'invalid' })
+  describe('is left', () => {
+    it('should return a left value if document is invalid', async () => {
+      const result = await sut.handle({ ...document, document: 'invalid' })
 
-    expect(result.isLeft()).toBeTruthy()
-  })
+      expect(result.isLeft()).toBeTruthy()
+    })
 
-  it('should return a left value if clientPhone is invalid', async () => {
-    const result = await sut.handle({ ...document, clientPhone: 'invalid' })
+    it('should return a left value if clientPhone is invalid', async () => {
+      const result = await sut.handle({ ...document, clientPhone: 'invalid' })
 
-    expect(result.isLeft()).toBeTruthy()
+      expect(result.isLeft()).toBeTruthy()
+    })
   })
 })
